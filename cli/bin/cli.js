10ce2f356cec5d4d4d03493f2bdf5e0db2e59ea7
Brandon Istenes <bistenes@gmail.com>
2020-03-26 22:17:31 -0700

    More minimal example

--- cli/bin/cli.js
+++ cli/bin/cli.js
@@ -1,27 +1,14 @@
-#!/usr/bin/env node
+const path = require("path");
 
-const program = require("commander");
-const version = require("../package.json").version;
-const autodoc = require("../lib/autodoc.js");
-
-program.version(version);
-program.option("-o, --outfile <outfile.md>", "change the output file", "README.md");
-program.option(
-  "-e, --entrypoint <entrypoint.js>",
-  "path to app entrypoint"
+require("ts-node").register(
+  Object.assign(
+    {
+      ignore: [/\.js/]
+    },
+    require("../tsconfig.json")
+  )
 );
-program.option(
-  "--cwd <cwd>",
-  "override the path to use as the current working directory"
-);
-program.option("-v, --verbose", "print lots of stuff");
 
-program.parse(process.argv);
+const entrypoint = process.argv[2];
 
-program.log = (...args) => console.log(...args);
-
-autodoc(program)
-  .catch(err => {
-    console.error(err);
-    process.exit(1);
-  });
+require(entrypoint);
